3173e4403e7d2463a6b8f5aa1ffa5cf38cf0ed9e
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-01-02 20:39:53 +0500

    fix: add url for test cypress_visit, add command cy;run

--- cypress/integration/cypress_visit.js
+++ cypress/integration/cypress_visit.js
@@ -1,7 +1,8 @@
+const url = Cypress.env("URL");
+
 describe('cy_visit_test', function() {
     it('finds the content "type"', function() {
-      cy.visit('https://rpguia.surge.sh')
-  
+      cy.visit(url)
       cy.contains('hype')
     })
   })
