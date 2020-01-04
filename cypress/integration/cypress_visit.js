ef79094ed925f1d07510a078df67dd9e5f507799
Ilnar Gilyaev <gilyaev.if@gmail.com>
2020-01-04 21:23:48 +0500

    fix: add Smokes.js test and del all another tests

--- cypress/integration/cypress_visit.js
+++
@@ -1,8 +0,0 @@
-
-
-describe('cy_visit_test', function() {
-    it('finds the content "type"', function() {
-      cy.visit(Cypress.env('URL'))
-      cy.contains('hype')
-    })
-  })
--- cypress/integration/example.js
+++
@@ -1,9 +0,0 @@
-
-/// <reference types="Cypress" />
-
-describe('My First Test', function() {
-    it('Visits the Kitchen Sink', function() {
-
-      expect(true).to.be.false
-    })
-  })
